cb71c26345f84ed179d41383eb0e39512a330c73
Trevor_Phillips <tr271v0r.phillips.industries@gmail.com>
2024-06-12 00:52:47 +0300

    add new slice: ControllerUSIA

---
+++ USIA/USIA/ControllerUSIA.cs
@@ -0,0 +1,38 @@
+﻿using AltV.Net;
+using USIA.Domain.Core;
+using USIA.Infrastructure.Business.Exceptions;
+using USIA.Infrastructure.Business;
+using USIA.Infrastructure.Data.Exceptions;
+
+namespace USIA
+{
+    internal class ControllerUSIA : IScript
+    {
+        public ServiceUSIA? serviceUSIA;
+        [ScriptEvent(ScriptEventType.PlayerConnect)]
+        public void UserConnect(User user, string reason)
+        {
+            serviceUSIA = new ServiceUSIA(user);
+            serviceUSIA.Show();
+        }
+
+        [ClientEvent("c:s:onAuthByLogin")]
+        public void OnAuthByLoginHandler(User user, string login, string password)
+        {
+            try
+            {
+                serviceUSIA!.AuthByLogin(login, password);
+                Console.WriteLine($"{login} ввёл верный пароль и залогинился!");
+            }
+            catch (ValidateException ve)
+            {
+                Console.WriteLine(ve.Message);
+            }
+            catch (ServerException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+
+        }
+    }
+}
--- USIA/USIA/USIA.cs
+++ USIA/USIA/USIA.cs
@@ -1,42 +1,9 @@
 ﻿using AltV.Net;
 using AltV.Net.Elements.Entities;
 using USIA.Domain.Core;
-using USIA.Infrastructure.Business;
-using USIA.Infrastructure.Business.Exceptions;
-using USIA.Infrastructure.Data.Exceptions;
 
 namespace USIA
 {
-    internal class Init : IScript
-    {
-        public ServiceUSIA? serviceUSIA;
-        [ScriptEvent(ScriptEventType.PlayerConnect)]
-        public void UserConnect(User user, string reason)
-        {
-            user.Emit("s:c:onAuthByLogin");
-            serviceUSIA = new ServiceUSIA();
-        }
-
-        [ClientEvent("c:s:onAuthByLogin")]
-        public void OnAuthByLoginHandler(User user, string login, string password)
-        {
-            try
-            {
-                serviceUSIA!.Login(login, password);
-                Console.WriteLine($"{login} ввёл верный пароль и залогинился!");
-            }
-            catch (ValidateException ve) 
-            {
-                Console.WriteLine(ve.Message);
-            }
-            catch (ServerException se)
-            {
-                Console.WriteLine(se.Message);
-            }
-            
-        }
-    }
-
     internal class USIA : Resource
     {
         public override IEntityFactory<IPlayer> GetPlayerFactory()
